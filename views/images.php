bc07c8e0998d432c368c470e19af0ffd94652cce
lily healey <lily@lilyhealey.co.uk>
2016-03-23 16:09:47 -0400

    add gallery launching

--- views/images.php
+++ views/images.php
@@ -1,14 +1,31 @@
 <?
 $media = $oo->media($uu->id);
-
+$image_urls = array();
 ?><div id="images"><?
+	$i = 0;
 	foreach($media as $m)
 	{
 		$url = m_url($m);
+		$image_urls[] = $url;
 		$caption = $m['caption'];
+		
 	?><div class="thumb">
-		<img src="<? echo $url; ?>">
+		<div onclick="launch(<? echo $i++; ?>);"><img src="<? echo $url; ?>"></div>
 		<div class="caption"><? echo $caption; ?></div>
 	</div><?
 	}
 ?></div>
+<div id="gallery" class="hidden" onclick="close_gallery();">
+	<img id="gallery-img" class="centre">
+</div>
+<script type="text/javascript" src="/static/js/gallery.js"></script>
+<script type="text/javascript">
+	var images = <? echo json_encode($image_urls); ?>;
+	var gallery_id = "gallery";
+	var gallery = document.getElementById(gallery_id);
+	var gallery_img = "gallery-img";
+	var attached = false;
+	var index = 0;
+	var in_gallery = false;
+	var non_gallery = ["main", "images"];
+</script>
