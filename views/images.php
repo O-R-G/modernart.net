8eebb3a430620e820bcdd6a187e67bf246cab478
lily healey <lily@lilyhealey.co.uk>
2016-03-28 14:53:51 -0400

    add preliminary full-screen code

--- views/images.php
+++ views/images.php
@@ -10,7 +10,7 @@ $image_urls = array();
 		$caption = $m['caption'];
 		
 	?><div class="thumb">
-		<div onclick="launch(<? echo $i++; ?>);"><img src="<? echo $url; ?>"></div>
+		<div onclick="launch(<? echo $i++; ?>);"><img src="<? echo $url; ?>" class="fullscreen"></div>
 		<div class="caption"><? echo $caption; ?></div>
 	</div><?
 	}
@@ -19,6 +19,7 @@ $image_urls = array();
 	<img id="gallery-img" class="centre">
 </div>
 <script type="text/javascript" src="/static/js/gallery.js"></script>
+<script type="text/javascript" src="/static/js/screenfull.js"></script>
 <script type="text/javascript">
 	var images = <? echo json_encode($image_urls); ?>;
 	var gallery_id = "gallery";
@@ -28,4 +29,14 @@ $image_urls = array();
 	var index = 0;
 	var in_gallery = false;
 	var non_gallery = ["main", "images"];
+	
+	var els = document.getElementsByClassName('fullscreen');
+	for(j = 0; j < els.length; j++)
+	{
+		els[j].addEventListener('click', function() {
+			if(screenfull.enabled) {
+				screenfull.toggle(els[j]);
+			}
+		});
+	}
 </script>
