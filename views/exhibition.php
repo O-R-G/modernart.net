05e863338970ddb22bcdb3655b9416baa8dbce39
lily healey <lily@lilyhealey.co.uk>
2016-04-07 14:35:33 -0400

    fix image launch on exhibition pages

--- views/exhibition.php
+++ views/exhibition.php
@@ -11,30 +11,34 @@ foreach($media as $m)
 $body = $item['body'];
 $children = $oo->children($uu->id);
 
-if($children)
-{
+
 ?><section id="exhibition" class="visible">
 	<header><? echo nl2br($item['name1']); ?></header><?
 	$url = $image_urls[0];
 	$caption = $media[0]['caption'];
+	if($url)
+	{
 	?><div class="thumb">
 		<div class="img-container" onclick="launch(<? echo $i++; ?>);">
 			<img src="<? echo $url; ?>" class="fullscreen">
 		</div>
 		<div class="caption"><? echo $caption; ?></div>
-	</div><? 
-	$cbody = $children[0]['body'];
-	$cbody = trim($cbody);
-	$cbody = strip_tags($cbody, "<i><b><a>");
-	$cbody = nl2br($cbody);
-	echo $cbody;
-?></section><?
-}
-else
-{
-?><div id="body" class="visible"><? echo nl2br($body); ?></div><?
-}
-?><div id="gallery" class="hidden" onclick="close_gallery();">
+	</div><?
+	}
+	if($children)
+	{
+		$cbody = $children[0]['body'];
+		$cbody = trim($cbody);
+		$cbody = strip_tags($cbody, "<i><b><a>");
+		$cbody = nl2br($cbody);
+		echo $cbody;
+	}
+	else
+	{
+		echo nl2br($body);
+	}
+?></section>
+<div id="gallery" class="hidden" onclick="close_gallery();">
 	<img id="gallery-img" class="centre">
 </div>
 <script type="text/javascript" src="/static/js/gallery.js"></script>
@@ -47,7 +51,7 @@ else
 	var attached = false;
 	var index = 0;
 	var in_gallery = false;
-	var non_gallery = ["main", "images", "body"];
+	var non_gallery = ["exhibition", "main"];
 	
 	var els = document.getElementsByClassName('fullscreen');
 	for(j = 0; j < els.length; j++)
