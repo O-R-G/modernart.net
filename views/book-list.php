55882a5386b20b9614a264a3b00d3e12938192fd
lily healey <lily@lilyhealey.co.uk>
2016-06-06 09:24:43 -0400

    remove author sections on book list

--- views/book-list.php
+++ views/book-list.php
@@ -7,21 +7,18 @@ $children = $oo->children($uu->id);
 		$tmp = trim(strip_tags($c['name1'], '<i><b>'));
 		$name = nl2br($tmp);
 		$grandchildren = $oo->children($c['id']);
-		?><section class="author">
-		    <!-- header><? echo $name; ?></header --><?
-		    foreach($grandchildren as $gc)
-		    {
-		        $u = $url."/".$gc['url'];
-		        $tmp = trim(strip_tags($gc['name1'], '<i><b>'));
-		        $name = nl2br($tmp);
-		        $m = $oo->media($gc['id']);
-		        $m_url = m_url($m[0]);
-                ?><div class="book">
-                    <div class="background-img" style="background-image:url('<? echo $m_url; ?>')"></div>
-                    <!-- img src="<? echo $m_url; ?>" -->
-                    <div class="caption"><? echo $name; ?></div>
-                </div><?
-		    }
-		?></section><?
+        foreach($grandchildren as $gc)
+        {
+            $u = $url."/".$gc['url'];
+            $tmp = trim(strip_tags($gc['name1'], '<i><b>'));
+            $name = nl2br($tmp);
+            $m = $oo->media($gc['id']);
+            $m_url = m_url($m[0]);
+            ?><div class="book">
+                <div class="background-img" style="background-image:url('<? echo $m_url; ?>')"></div>
+                <!-- img src="<? echo $m_url; ?>" -->
+                <div class="caption"><? echo $name; ?></div>
+            </div><?
+        }
 	}
 ?></div>
