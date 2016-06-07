5a88fe360b97f67057a074fffa630f5eb702d1d6
lily healey <lily@lilyhealey.co.uk>
2016-06-07 06:58:22 -0400

    news, again

--- views/news.php
+++ views/news.php
@@ -7,13 +7,7 @@ for ($i = 0; $i < 2; $i++)
     $grandchildren = $oo->children($child['id']);
 ?><div class="col">
     <header><? echo $child['name1']; ?></header><?
-	foreach($grandchildren as $gc)
-	{
-		$url = $uu->url."/".$child['url']."/".$gc['url'];
-		// $tmp = trim(strip_tags($c['name1'], '<i><b>'));
-		$name = nl2br($gc['name1']);
-		?><div class="news-story"><? echo $name; ?></div><?
-	}
+    echo $child['body'];
 ?></div><?
 }
 ?></div>
