86de0f83505fc52a0243937564f4acd83036178c
david reinfurt <reinfurt@Adriano-Olivetti.local>
2017-03-10 17:52:59 +0100

    add ignore link function using : in children.php

--- views/children.php
+++ views/children.php
@@ -2,8 +2,7 @@
 $children = $oo->children($uu->id);
 $n = count($children);
 
-?>
-<div id="children" class="centered centeralign">
+?><div id="children" class="centered centeralign">
     <div class="col"><?
 	for ($i = 0; $i < ($n / 2); $i++)
 	{
@@ -11,14 +10,14 @@ $n = count($children);
 		$url = $uu->url."/".$c['url'];
 		$tmp = trim(strip_tags($c['name1'], '<i><b>'));
 		$name = nl2br($tmp);
-		if($tmp == $name)
-		{
-		?><div><a href="<? echo $url; ?>"><? echo $name; ?></a></div><?
-		}
-		else
-		{
-		?><div><p><a href="<? echo $url; ?>"><? echo $name; ?></a></p></div><?
-		}
+        if($tmp != $name) $ptags = true;
+        if (substr($name, 0, 1) == ":") $url = null;
+        $name = ltrim($name, ":");
+        ?><div>
+            <? echo ($ptags) ? "<p>" : ""; ?>
+            <a href="<? echo $url; ?>"><? echo $name; ?></a>
+            <? echo ($ptags) ? "</p>" : ""; ?>
+        </div><?
 	}
 ?></div>
 <div class="col"><?
@@ -28,14 +27,28 @@ $n = count($children);
 		$url = $uu->url."/".$c['url'];
 		$tmp = trim(strip_tags($c['name1'], '<i><b>'));
 		$name = nl2br($tmp);
-		if($tmp == $name)
-		{
-		?><div><a href="<? echo $url; ?>"><? echo $name; ?></a></div><?
+        if($tmp != $name) $ptags = true;
+        if (substr($name, 0, 1) == ":") $url = null;
+        $name = ltrim($name, ":");
+        ?><div>
+            <? echo ($ptags) ? "<p>" : ""; ?>
+            <a href="<? echo $url; ?>"><? echo $name; ?></a>
+            <? echo ($ptags) ? "</p>" : ""; ?>
+        </div><?
+
+
+
+/*
+	    $c = $children[$i];
+		$url = $uu->url."/".$c['url'];
+		$tmp = trim(strip_tags($c['name1'], '<i><b>'));
+		$name = nl2br($tmp);
+		if($tmp == $name) {
+		    ?><div><a href="<? echo $url; ?>"><? echo $name; ?></a></div><?
+		} else {
+		    ?><div><p><a href="<? echo $url; ?>"><? echo $name; ?></a></p></div><?
 		}
-		else
-		{
-		?><div><p><a href="<? echo $url; ?>"><? echo $name; ?></a></p></div><?
-		}
+*/
 	}
 ?></div>
 </div>
